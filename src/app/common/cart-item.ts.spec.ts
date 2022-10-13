4e004d6721e8452dbc9683bd5606aef196359b25
jathiswarbhaskar <codguy2802@gmail.com>
2022-10-13 21:05:00 +0530

    Intial

---
+++ src/app/common/cart-item.ts.spec.ts
@@ -0,0 +1,7 @@
+import { CartItem.Ts } from './cart-item.ts';
+
+describe('CartItem.Ts', () => {
+  it('should create an instance', () => {
+    expect(new CartItem.Ts()).toBeTruthy();
+  });
+});
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default{
+    oidc:{
+        clientId:'0oa6tcle0j4BFJ01d5d7', 
+        issuer:'https://dev-21872987.okta.com/oauth2/defau',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes : ['openid','profile','email']
+    }
+}
