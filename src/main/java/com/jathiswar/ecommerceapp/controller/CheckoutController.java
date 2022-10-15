541995f522990f08b083503096acd56f5dc3939f
jathiswarbhaskar <codguy2802@gmail.com>
2022-10-15 23:33:53 +0530

    Https

--- src/main/java/com/jathiswar/ecommerceapp/controller/CheckoutController.java
+++ src/main/java/com/jathiswar/ecommerceapp/controller/CheckoutController.java
@@ -6,22 +6,24 @@ import com.jathiswar.ecommerceapp.dto.PurchaseResponse;
 import com.jathiswar.ecommerceapp.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
+
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
+
     private CheckoutService checkoutService;
 
-    public CheckoutController(CheckoutService checkoutService){
+    public CheckoutController(CheckoutService checkoutService) {
         this.checkoutService = checkoutService;
     }
 
     @PostMapping("/purchase")
-    public PurchaseResponse purchaseResponse(@RequestBody Purchase purchase){
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
     }
-
 
 }
